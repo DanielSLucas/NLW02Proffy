80300a9747b9807e7d661734ec940f5ae748a315
DanielSLucas <daniellucas-pms@hotmail.com>
2020-08-25 16:26:40 -0300

    fix

--- web/src/hooks/auth.tsx
+++ web/src/hooks/auth.tsx
@@ -37,6 +37,7 @@ export const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@Proffy:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return {
         token,
         user: JSON.parse(user),
